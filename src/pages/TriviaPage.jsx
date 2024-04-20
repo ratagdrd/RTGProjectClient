83113539a9751c3fc4c94bd6af16888cbdfbc568
DanielBZ-2024 <155885241+DanielBZ-2024@users.noreply.github.com>
2024-04-20 13:43:30 +0300

    SERVER QUES

--- src/pages/TriviaPage.jsx
+++ src/pages/TriviaPage.jsx
@@ -5,25 +5,70 @@ import pic1 from "../images/HordusPort.jpg";
 import ArrowForwardIosOutlinedIcon from '@mui/icons-material/ArrowForwardIosOutlined';
 import { IconButton } from "@mui/material";
 import FooterGraphic from "../FuncComp/FooterGraphic";
+import { useEffect } from "react";
 
-export default function TriviaPage({ ques, ans, prog, tot, correctAns }) {
-  const question = " איפה היה ארמון הורדוס בלה בלה בלה בלה ";
-  const answersList = ["תשובה 1 בלה בלה", "תשובה 2 בלה בלה", "תשובה 3 בלה בלה", "תשובה 4 בלה בלה"];  //להחליף כשמגיע דאטה מהשרת
-  const percentage = (1 / 10) * 100;
-
+export default function TriviaPage() {
+  const [question, setQuestion] = useState("");
+  const [ansList, setAnsList] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [correctAns, setCorrectAns] = useState(0);
+  const [quesNum, setQuesNum] = useState(0);
   const [selectedAnswer, setSelectedAnswer] = useState(null);
-  const correctAnswerIndex = 1; 
+
+
+  useEffect(() => {
+    onLoad();
+  }, []);
+
+
+  const onLoad = () => {
+    fetch("https://localhost:7052/api/QuestionForActivity/1", {
+      method: 'GET',
+      headers: new Headers({
+        'Content-Type': 'application/json; charset=UTF-8',
+      })
+    })
+      .then(res => {
+        console.log('res=', res);
+        console.log('res.status', res.status);
+        console.log('res.ok', res.ok);
+        return res.json()
+      })
+      .then(
+        (result) => {
+          console.log("fetch result: ", result);
+          fillTrivia(result);
+          // Process the result as needed
+        },
+        (error) => {
+          console.log("Error fetching trivia data:", error);
+        }
+      );
+  };
+
+
+  const fillTrivia = (data) => {
+    setQuestion(data[0]["question"]);
+    setAnsList([data[0]["answer1"], data[0]["answer2"], data[0]["answer3"], data[0]["answer4"]]);
+    setCorrectAns(data[0]["correctedAnswer"]);
+    setQuesNum(data[0]["questionNo"]);
+
+  }
+
+
+  const percentage = (quesNum / 10) * 100;
+
 
   const handleAnswerClick = (index) => {
     debugger
-      setSelectedAnswer(index);
-      const buttons = document.querySelectorAll('.buttons-game');
-      buttons.forEach((button, i) => {
-        if ( i !== correctAnswerIndex) {
-          button.classList.add('wrong-ans');
-        }
-      });
-    
+    setSelectedAnswer(index);
+    const buttons = document.querySelectorAll('.buttons-game');
+    buttons.forEach((button, i) => {
+      if (i !== correctAns) {
+        button.classList.add('wrong-ans');
+      }
+    });
+
   };
 
 
@@ -46,17 +91,17 @@ export default function TriviaPage({ ques, ans, prog, tot, correctAns }) {
       </div>
       <br />
       <div className="answers-container">
-        {answersList.map((answer, index) => (
+        {ansList.map((answer, index) => (
           <button
-          key={index}
-          className={`buttons-game correct-ans`}
-          onClick={() => handleAnswerClick(index)}
-        >
-          {answer}
-        </button>
+            key={index}
+            className={`buttons-game correct-ans`}
+            onClick={() => handleAnswerClick(index)}
+          >
+            {answer}
+          </button>
         ))}
       </div>
-      <FooterGraphic/>
+      <FooterGraphic />
     </div>
   );
 }
