36c4cdad2f3f94e634cff12a8ae2c2e215411ca5
RoniBenBaruch <ronibenbaruch@gmail.com>
2024-04-24 17:21:53 +0300

    connecting rating to db

--- src/pages/RatingPage.jsx
+++ src/pages/RatingPage.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import "./../css/GeneralPages.css";
 
@@ -6,9 +6,69 @@ import Rating from '@mui/material/Rating';
 
 import Header from "../FuncComp/Header";
 import FooterGraphic from "../FuncComp/FooterGraphic";
+import { log } from "three/examples/jsm/nodes/Nodes.js";
 
 export default function RatingPage() {
+
     const txtToHeader = "דעתכם חשובה לנו! ";
+
+    const [activities, setActivities] = useState([]);
+    const [ratings, setRatings] = useState([]);
+
+    useEffect(() => {
+        onLoad();
+    }, []);
+
+    const onLoad = () => {
+        // Fetch from Activity Data table
+        fetch("https://localhost:7052/api/Activity", {
+            method: 'GET',
+            headers: new Headers({
+                'Content-Type': 'application/json; charset=UTF-8',
+            })
+        })
+            .then(res => res.json())
+            .then(
+                (activities) => {
+                    console.log("Activities fetch result: ", activities);
+                    setActivities(activities); // Set activities state based on the fetched data
+                },
+                (error) => {
+                    console.log("Error fetching activity data:", error);
+                }
+            );
+    };
+
+
+    const handleRatingChange = (index, value) => {
+        const newRatings = [...ratings];
+        newRatings[index] = value;
+        setRatings(newRatings);
+        console.log(newRatings);
+
+         // Update rating in the database
+         updateRatingInDatabase(activities[index].activitycode, value);
+    };
+
+    const updateRatingInDatabase = (activitycode, newRate) => {
+        fetch(`https://localhost:7052/api/Activity/${activitycode}/${newRate}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json; charset=UTF-8',
+            },
+            body: JSON.stringify({ rate: newRate }),
+        })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Failed to update rating in the database');
+                }
+                console.log(`Rating for activity ${activitycode} updated successfully.`);
+            })
+            .catch(error => {
+                console.error('Error updating rating in the database:', error);
+            });
+    };
+
 
     return (
 
@@ -18,21 +78,21 @@ export default function RatingPage() {
                 <h5>נשמח אם תוכלו לדרג את הפעילויות בתחנות הבאות</h5>
             </div>
             <div className="rating-container">
-                <p className="rating-station">
-                    <Rating className="rating-stars" name="no-value" value={null} />
-                    <span className="rating-text"> אמפי תיאטרון</span>
-                </p>
-                <p className="rating-station">
-                    <Rating className="rating-stars" name="no-value" value={null} />
-                    <span className="rating-text">היפודרום</span>
-                </p>
-                <p className="rating-station">
-                    <Rating className="rating-stars" name="no-value" value={null} />
-                    <span className="rating-text">ארמון הצוק</span>
-                </p>
+                {activities.map((activity, index) => (
+                    <p className="rating-station" key={index}>
+                       <Rating
+                            className="rating-stars"
+                            name={`rating-${index}`}
+                            value={ratings[index] || 0} // Default to 0 if the rating is not yet defined
+                            onChange={(event, value) => handleRatingChange(index, value)}
+                            precision={1} // Allow rating to be selected in increments of 1
+                        />
+                        <span className="rating-text">{activity.activityname}</span>
+                    </p>
+                ))}
             </div>
             <div className="inner-container">
-            <h5>תודה! נתראה בפעם הבאה!</h5>
+                <h5>תודה! נתראה בפעם הבאה!</h5>
             </div>
             <div><FooterGraphic /></div>
         </div>
