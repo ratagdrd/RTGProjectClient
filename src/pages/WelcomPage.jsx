eee8f8d11e6dd04ca04cecfb9f192e16dedc7c41
RoniBenBaruch <ronibenbaruch@gmail.com>
2024-04-29 18:50:19 +0300

    fixing css 1 and adding site details

--- src/pages/WelcomPage.jsx
+++ src/pages/WelcomPage.jsx
@@ -1,22 +1,91 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
 import Header from "../FuncComp/Header";
 import MainContent from "../FuncComp/MainContent";
 import MainButton from "../FuncComp/MainButton";
 import FooterGraphic from "../FuncComp/FooterGraphic";
+import Button from '@mui/material/Button';
+
+import "./../css/Games.css";
+import "./../css/GeneralPages.css";
 
 export default function WelcomPage() {
+  const [siteDetails, setSiteDetails] = useState(null);
+  const [showDetails, setShowDetails] = useState(false);
+
+  useEffect(() => {
+    onLoad();
+  }, []);
+
+
+  const onLoad = () => {
+    fetch('https://localhost:7052/api/Site/1', {
+      method: 'GET',
+      headers: new Headers({
+        'Content-Type': 'application/json; charset=UTF-8',
+        'Accept': 'application/json; charset=UTF-8',
+      })
+    })
+      .then(res => {
+        console.log('res=', res);
+        console.log('res.status', res.status);
+        console.log('res.ok', res.ok);
+        return res.json()
+      })
+      .then(
+        (site) => {
+          console.log("fetch result: ", site);
+          setSiteDetails(site);
+        },
+        (error) => {
+          console.log("Error fetching word data:", error);
+        }
+      );
+  }
   const txtToBtn = "יצירת קבוצה";
   const txtTomainContent = "!צרו קבוצה והתחילו לשחק";
   const txtToHeader = "ברוכים הבאים לאתר קיסריה";
-  const question = "1234";
-  const answersList = ["a", "b", "c", "d"];
+
+  const handleShowDetails = () => {
+    setShowDetails(true);
+  };
+
+  const handleCloseDetails = () => {
+    setShowDetails(false);
+  };
+
   return (
     <div>
-      <Header textToHeader={txtToHeader} />
-      <MainContent textToMainContent={txtTomainContent} />
-      <MainButton textToBtn={txtToBtn} navigateTo={"/register"} />
-      <MainButton textToBtn={"התחל טריוויה"} navigateTo={"/Trivia"} />
+      <div className="welcome-header"><Header textToHeader={txtToHeader} /></div>
+      <div className="welcome-content" ><MainContent textToMainContent={txtTomainContent} /></div>
+      <div className="welcome-buttons"><MainButton textToBtn={txtToBtn} navigateTo={"/register"} />
+        <MainButton textToBtn={"התחל טריוויה"} navigateTo={"/Trivia"} /></div>
+      <Button variant="text" className="welcome-site" onClick={handleShowDetails}>לחץ לצפייה בפרטי האתר</Button>
       <FooterGraphic />
+      {showDetails && (
+        <div className="game-over-popup">
+          <div>
+            <h4 className='game-over-header'>פרטי האתר</h4>
+            <p className='game-over-p'>
+              <span className="bold-text">שם האתר:</span> {siteDetails?.siteName}
+            </p>
+            <p className='game-over-p'>
+              <span className="bold-text">תיאור:</span> {siteDetails?.sDescription}
+            </p>
+            <p className='game-over-p'>
+              <span className="bold-text">טלפון:</span> {siteDetails?.phoneNo}
+            </p>
+            <p className='game-over-p'>
+              <span className="bold-text">שעות פתיחה:</span> {siteDetails?.openingHours}
+            </p>
+            <p className='game-over-p'>
+              <span className="bold-text">אתר אינטרנט:</span>{" "}
+              <a href={siteDetails?.webSite} target="_blank">{siteDetails?.webSite}</a>
+            </p>
+          </div>
+
+          <button onClick={handleCloseDetails}>סגור</button>
+        </div>
+      )}
     </div>
   );
 }
