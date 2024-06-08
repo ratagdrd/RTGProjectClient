9edc51be61921ec8d582e4cd00a5bc6b5a5697a5
RoniBenBaruch <ronibenbaruch@gmail.com>
2024-06-08 12:47:25 +0300

    admin

--- src/pages/DataTablePage.jsx
+++ src/pages/DataTablePage.jsx
@@ -7,13 +7,28 @@ import IconButton from '@mui/material/IconButton';
 import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
 import "./../css/AdminPages.css";
+import Header from "../FuncComp/Header";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import rtlPlugin from "stylis-plugin-rtl";
+import { prefixer } from "stylis";
+
+import { CacheProvider } from "@emotion/react";
+import createCache from "@emotion/cache";
 
 export default function DataTablePage() {
+    const Logo = "https://proj.ruppin.ac.il/cgroup60/test2/tar1/Images/footer/logo.png";
     const [selectedTable, setSelectedTable] = useState('');
     const [tableData, setTableData] = useState([]);
     const [currentPage, setCurrentPage] = useState(1);
     const rowsPerPage = 20;
     const tableNames = ["Spot", "Activity", "QuestionForActivity"];
+    const txtToHeader = "מערכת ניהול";
+    const theme = createTheme({ direction: "rtl" });
+
+    const cacheRtl = createCache({
+      key: "muirtl",
+      stylisPlugins: [prefixer, rtlPlugin],
+    });
 
     useEffect(() => {
         document.body.className = 'adminPage';
@@ -68,73 +83,92 @@ export default function DataTablePage() {
     const currentPageData = tableData.slice((currentPage - 1) * rowsPerPage, currentPage * rowsPerPage);
 
     return (
-        <div className="container">
-            <FormControl>
-                <Select
-                    labelId="table-select-label"
-                    value={selectedTable}
-                    onChange={handleTableSelect}
-                    displayEmpty
-                    inputProps={{ 'aria-label': 'Without label' }}
-                >
-                    <MenuItem value="">בחר טבלה</MenuItem>
-                    {tableNames.map((tableName) => (
-                        <MenuItem key={tableName} value={tableName}>
-                            {tableName}
-                        </MenuItem>
-                    ))}
-                </Select>
-            </FormControl>
-            {selectedTable && tableData.length > 0 && (
-                <div>
-                    <Table striped bordered hover>
-                        <thead>
-                            <tr>
-                                {Object.keys(tableData[0]).map(field => (
-                                    <th key={field}>{field}</th>
-                                ))}
-                                {selectedTable === "Activity" && <th>Average Rate</th>}
-                                <th>Actions</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {currentPageData.map((row, index) => (
-                                <tr key={index}>
-                                    {Object.keys(row).map(field => (
-                                        <td key={field}>{row[field]}</td>
-                                    ))}
-                                    {selectedTable === "Activity" && (
-                                        <td>{calculateAverageRate(row)}</td>
-                                    )}
-                                    <td>
-                                        <IconButton color="info" onClick={() => handleEdit(row.id)}>
-                                            <EditIcon />
-                                        </IconButton>
-                                        <IconButton color="error" onClick={() => handleDelete(row.id)}>
-                                            <DeleteIcon />
-                                        </IconButton>
-                                    </td>
-                                </tr>
-                            ))}
-                        </tbody>
-                    </Table>
-                    <Pagination>
-                        <Pagination.First onClick={() => setCurrentPage(1)} disabled={currentPage === 1} />
-                        <Pagination.Prev onClick={() => setCurrentPage(prev => Math.max(prev - 1, 1))} disabled={currentPage === 1} />
-                        {Array.from({ length: totalPages }, (_, index) => (
-                            <Pagination.Item
-                                key={index + 1}
-                                active={index + 1 === currentPage}
-                                onClick={() => setCurrentPage(index + 1)}
-                            >
-                                {index + 1}
-                            </Pagination.Item>
-                        ))}
-                        <Pagination.Next onClick={() => setCurrentPage(prev => Math.min(prev + 1, totalPages))} disabled={currentPage === totalPages} />
-                        <Pagination.Last onClick={() => setCurrentPage(totalPages)} disabled={currentPage === totalPages} />
-                    </Pagination>
+        <CacheProvider value={cacheRtl}>
+        <ThemeProvider theme={theme}>
+        <div className="outer-container-admin">
+            <nav className="navbar navbar-fixed-top">
+                <div className="navbar-logo">
+                    <img src={Logo} alt="Logo"  style={{ width: "60px" }}/>
                 </div>
-            )}
+                <div className="navbar-header">
+                <Header textToHeader={txtToHeader}></Header>
+                </div>
+                <FormControl>
+                    <Select
+                    className='selectTable'
+                        labelId="table-select-label"
+                        value={selectedTable}
+                        onChange={handleTableSelect}
+                        displayEmpty
+                        inputProps={{ 'aria-label': 'Without label' }}
+                    >
+                        <MenuItem value="">בחר טבלה</MenuItem>
+                        {tableNames.map((tableName) => (
+                            <MenuItem key={tableName} value={tableName}>
+                                {tableName}
+                            </MenuItem>
+                        ))}
+                    </Select>
+                </FormControl>
+            </nav>
+            <div className="container">
+                {selectedTable && tableData.length > 0 && (
+                    <div>
+                        <Table striped bordered hover>
+                            <thead>
+                                <tr>
+                                    {Object.keys(tableData[0]).map((field, index) => (
+                                        <th key={field} style={{ order: selectedTable === "Activity" ? (Object.keys(tableData[0]).length - index) : index }}>
+                                            {field}
+                                        </th>
+                                    ))}
+                                    {selectedTable === "Activity" && <th>Average Rate</th>}
+                                    <th>Actions</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {currentPageData.map((row, index) => (
+                                    <tr key={index}>
+                                        {Object.keys(row).map((field, index) => (
+                                            <td key={field} style={{ order: selectedTable === "Activity" ? (Object.keys(tableData[0]).length - index) : index }}>
+                                                {row[field]}
+                                            </td>
+                                        ))}
+                                        {selectedTable === "Activity" && (
+                                            <td>{calculateAverageRate(row)}</td>
+                                        )}
+                                        <td>
+                                            <IconButton color="info" onClick={() => handleEdit(row.id)}>
+                                                <EditIcon />
+                                            </IconButton>
+                                            <IconButton color="error" onClick={() => handleDelete(row.id)}>
+                                                <DeleteIcon />
+                                            </IconButton>
+                                        </td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </Table>
+                        <Pagination>
+                            <Pagination.First onClick={() => setCurrentPage(1)} disabled={currentPage === 1} />
+                            <Pagination.Prev onClick={() => setCurrentPage(prev => Math.max(prev - 1, 1))} disabled={currentPage === 1} />
+                            {Array.from({ length: totalPages }, (_, index) => (
+                                <Pagination.Item
+                                    key={index + 1}
+                                    active={index + 1 === currentPage}
+                                    onClick={() => setCurrentPage(index + 1)}
+                                >
+                                    {index + 1}
+                                </Pagination.Item>
+                            ))}
+                            <Pagination.Next onClick={() => setCurrentPage(prev => Math.min(prev + 1, totalPages))} disabled={currentPage === totalPages} />
+                            <Pagination.Last onClick={() => setCurrentPage(totalPages)} disabled={currentPage === totalPages} />
+                        </Pagination>
+                    </div>
+                )}
+            </div>
         </div>
+        </ThemeProvider>
+        </CacheProvider>
     );
 }
